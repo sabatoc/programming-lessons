867688ebdc756e22e448391571e303619a911957
Claudio Sabato <claudiosabato@Claudios-MacBook-Air.local>
2024-05-08 23:21:48 +0100

    Finalised game 2

--- websites/school-project/js/gioco2.js
+++ websites/school-project/js/gioco2.js
@@ -1,29 +1,35 @@
-function startGame() {
-    let countdown = 15;
-    const timerElement = document.getElementById('timer');
+let countdown = 15;
+const timerElement = document.getElementById('timer');
+timerElement.textContent = countdown;
+    
+const interval = setInterval(function() {
+    countdown -= 1;
     timerElement.textContent = countdown;
-    
-    const interval = setInterval(function() {
-        countdown -= 1;
-        timerElement.textContent = countdown;
-        if (countdown <= 0) {
-            clearInterval(interval);
-            timerElement.style.display = 'none';  // Hide timer
-        }
-    }, 1000);
-    
-    setTimeout(() => {
-        const button = document.createElement('button');
-        button.textContent = 'Click when you think 15 seconds are up!';
-        button.onclick = function() {
-            if (countdown <= 0) {
-                alert('Congratulations, you timed it perfectly!');
-            } else {
-                alert('Too soon! Try again!');
-            }
-        };
-        document.getElementById('gameContainer').appendChild(button);
-    }, 15000);  // Show the button after 15 seconds
-}
+    if (countdown < 13) {
+        timerElement.style.display = 'none';  // Hide timer
+    }
+    if (countdown == -1) {
+        timerElement.textContent = countdown + 1; // This way we show 0 at the end of the game
+        timerElement.style.display = 'block';  // Show timer when 0 seconds are reached
 
-document.getElementById('topBar').getElementsByTagName('button')[0].disabled = false;
+        const endGameInterval = setInterval(function() {
+            alert('Troppo tardi, prova di nuovo!');
+            clearInterval(interval);
+            clearInterval(endGameInterval)
+            window.location.reload();
+        }, 20);
+    }
+}, 1000);
+    
+const button = document.getElementById('guessButton');
+button.onclick = function() {
+    if (countdown == 0) {
+        alert('Congratulazioni, hai un tempismo perfetto!');
+        clearInterval(interval);
+        window.location.reload();
+    } else {
+        alert('Troppo presto, prova di nuovo!');
+        clearInterval(interval);
+        window.location.reload();
+    }
+};
